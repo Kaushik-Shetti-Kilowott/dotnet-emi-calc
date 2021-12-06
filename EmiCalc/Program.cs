9e871de2ddfe21f9b0ec2e984abb92218be5bd6d
Kaushik-Shetti-Kilowott <kaushik.shetti@kilowott.com>
2021-12-06 12:04:48 +0530

    commit

--- EmiCalc/Program.cs
+++ EmiCalc/Program.cs
@@ -4,33 +4,39 @@ namespace EmiCalc
 {
     internal class Program
     {
-        internal class EmiCalc
+       
+        static void Main(string[] args)
         {
-            public double EmiCalculator(double p, double i, double t, double n)
-            {
-                double emi;
-                emi = p * Math.Pow((1 + i / n), (t * n));
-                return (emi) / 24;
-            }
-        }
-            static void Main(string[] args)
-        {
-            var sp=new EmiCalc();
             double p, i, n, t, emi;
-            Console.WriteLine("Enter the principle:");
-            p = Convert.ToDouble(Console.ReadLine());
-            //Console.WriteLine($"Principlal is {p}");
-            Console.WriteLine("Enter the interest rate");
-            i = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter the loan duration");
-            t = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter the monthly compounding");
-            n = Convert.ToDouble(Console.ReadLine());
-            //emi = emi_calculator(p, i, n, t);
-            emi = sp.EmiCalculator(p, i, t, n);
-            Console.WriteLine("Monthly EMI is =" +Math.Round(emi,2) + " INR");
+            var sp=new EmiCalculator();
+            try
+            {
+               
+                Console.WriteLine("Enter the principle:");
+                p = Convert.ToDouble(Console.ReadLine());
+                //Console.WriteLine($"Principlal is {p}");
+                Console.WriteLine("Enter the interest rate");
+                i = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter the loan duration");
+                t = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter the monthly compounding");
+                n = Convert.ToDouble(Console.ReadLine());
+                emi = sp.CalculateEmi(p, i, t, n);
+                Console.WriteLine("Monthly EMI is =" + Math.Round(emi, 2) + " INR");
 
-         }
+            }
+            catch (FormatException e)
+            {
+                
+                Console.WriteLine("Enter valid integer");
+            }
+            finally
+            {
+                Console.WriteLine("Please re run the program and pass only valid input");
+
+            }
+         
+        }
     }
 }
 
